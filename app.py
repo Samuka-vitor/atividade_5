6efe59db04f8dbb4ab2e65e1ea6380c859e007c0
Samuka-Vitor <samuelvitor93andrade@gmail.com>
2024-09-27 21:17:26 -0300

    versão 1.0

---
+++ app.py
@@ -0,0 +1,10 @@
+# crie uma lista com 15 nomes de cidades, e exiba na tela em ordem alfabética. Ao terminar, envie para o GitHub.
+cidades = ["Brasil", "Argentina", "Canadá", "Japão", "Austrália", "Alemanha", "França", "Itália", "Espanha", "Portugal", "México", "Índia", "China", "Rússia", "Egito"]
+
+cidades.sort()
+
+for cidade in cidades:
+    print(cidade)
+
+
+    
